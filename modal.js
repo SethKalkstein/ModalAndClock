34a244c94d8e68a7af5a2e2dcc39bcb434fb2187
SethKalkstein <sethkal7@yahoo.com>
2018-12-31 19:14:51 -0500

    cleaned up the code a little

--- modal.js
+++ modal.js
@@ -1,26 +1,34 @@
 
 function createBox1(){
 	var box1 = document.createElement("div");
+	
 	box1.style.width = "300px";
-	box1.style.height = "100px";
+	box1.style.height = "130px";
 	box1.style.backgroundColor = "white";
 	box1.style.border= "1px solid black";
 	box1.style.position = "absolute";
 	box1.style.top = "100px";
 	box1.style.right = "200px"
 	box1.style.borderRadius = "5px";
+
 	document.body.appendChild(box1);
+	
 	var box2 = createBox2();
+	
 	box1.appendChild(box2);
+	
 	box2.addEventListener("click",function(){
-	box1.style.display = "none";
+		box1.style.display = "none";
 	})
+
 	var boxText = document.createElement("p");
 	boxText.innerHTML = "anim id est laborum. Lorem ipsum dolorlore mlorem est laborum. Lorem ipsum dolorlorem.";
 	box1.appendChild(boxText);
 	var boxLink = document.createElement("a");
-	boxLink.setAttribute("href","clock.html");
+	boxLink.setAttribute("href", "index.html");
+
 	boxLink.innerHTML = "Click here to go to the clock";
+	
 	box1.appendChild(boxLink);
 	boxText.style.padding = "10px";
 	boxLink.style.padding = "10px";
@@ -28,28 +36,29 @@ function createBox1(){
 
 
 function modalPopup(){
-	setTimeout(createBox1, 3000)
+	setTimeout(createBox1, 1000)
 }
 window.addEventListener("load", modalPopup);
 
-
+//
  function createBox2(){
  	var box2 = document.createElement("div");
+
  	box2.style.width = "15px";
- 	box2.style.height = "15px";
- 	// box2.style.backgroundColor = "red";
+ 	box2.style.height = "17px";
+
  	box2.style.borderLeft= "1px solid black";
  	box2.style.borderBottom= "1px solid black";
 	box2.style.position = "relative";
-	// box2.style.borderRadius = "5px";
+
 	box2.style.float= "right";
 	box2.innerHTML = "X";
-	box2.style.padding = "auto";
+	box2.style.paddingLeft = "3px";
+
 	box2.addEventListener("mouseover",function(){
 		box2.style.cursor= "pointer";
 	})
+
  	return box2;
  }
 
-// box1.appendChild(box2)
-// box2.appendChild(boxText)
